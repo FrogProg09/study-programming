64222a7e8c2d6029f7d4442f4e50452388ff1abd
Sviatoslav Vavilov <frogprog09@gmail.com>
2026-04-25 19:09:06 +0400

    Solved task 7

--- cpp/lms-yandex/lvl0/step6/task7/solution.cpp
+++ cpp/lms-yandex/lvl0/step6/task7/solution.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
-int main (){
+int main() {
 
-    return 0;
+    char symbolReg;
+    int moveStep;
+    cin >> symbolReg >> moveStep;
+
+    if ( 32 <= (int)symbolReg && (int)symbolReg <= 126){
+
+        char symbolProcessed;
+
+        // looping back to 32th and so on symbol (126 - 32 = 95)
+        if ((int)symbolReg + moveStep > 126) {
+            symbolReg = (int)symbolReg - 95;
+            symbolProcessed = (int)symbolReg + moveStep;
+            symbolReg = (int)symbolReg + 95;
+        }
+        else{
+            symbolProcessed = (int)symbolReg + moveStep;
+        }
+
+        cout << "Исходный символ: " << symbolReg << " (код: " << (int)symbolReg << ")\n";
+        cout << "Новый символ: " << symbolProcessed << " (код: " << (int)symbolProcessed << ")\n";
+
+        return 0;
+    }
+    return 1;
 }
