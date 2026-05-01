c96abbfb73fc1933ece9e1ea56e3029ba7b5caff
Sviatoslav Vavilov <frogprog09@gmail.com>
2026-05-01 09:51:52 +0400

    Solved task 2

--- cpp/lms-yandex/lvl0/step7/task2/solution.cpp
+++ cpp/lms-yandex/lvl0/step7/task2/solution.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    // format of commandNameCompressed: City_Name
+    string commandNameCompressed;
+    cin >> commandNameCompressed;
+
+    char breakPoint = commandNameCompressed.find("_");
+
+    // format of commandNameReg: Name (without City_)
+    string commandNameReg = commandNameCompressed.substr(breakPoint + 1, 700);
+    cout << commandNameReg << "! " << commandNameReg << "! Go!\n";
+    return 0;
+}
