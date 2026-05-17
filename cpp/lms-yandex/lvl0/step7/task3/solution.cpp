da28530ebd10de0b2603e039a91e69977dbf3e20
Sviatoslav Vavilov <frogprog09@gmail.com>
2026-05-17 20:53:55 +0400

    Added makefiles for each task

--- cpp/lms-yandex/lvl0/step7/task3/solution.cpp
+++ cpp/lms-yandex/lvl0/step7/task3/solution.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    string speciesOne, speciesTwo;
+    cin >> speciesOne >> speciesTwo;
+
+    string speciesFinal;
+    speciesFinal += speciesOne[speciesOne.length() / 2];
+    speciesFinal += speciesTwo[speciesTwo.length() % 2];
+
+    cout << speciesFinal;
+    return 0;
+}
